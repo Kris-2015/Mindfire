9d20667b52f3c791e70484deac7dbd6506e3d8ef
Kris-2015 <krishnadev.rnc@gmail.com>
2016-06-14 19:01:00 +0530

    Added new files and folders

--- Profile_App/db.php
+++ Profile_App/db.php
@@ -1,22 +1,8 @@
 <?php
 ini_set('display_errors', '1');
 
-$hostname = "localhost";
-$username = "root";
-$pass     = "mindfire";
-$db_name  = "employee";
-
-
-// Create connection
-
-$conn = mysqli_connect($hostname, $username, $pass, $db_name);
-
-// Check connection
-
-if (!$conn) {
-    die("Connection failed:" . mysqli_error());
-}
-//echo "Connected Successfully";
+include('db_conn.php');
+include('image_upload.php');
 
 if(isset($_POST['submit'])) {
     // variable declaration
@@ -71,12 +57,13 @@ if(isset($_POST['submit'])) {
     
     $communication = implode(',', $_POST['communication']);
 
-    include("image_upload.php");
+    fileUplod($_FILES);
     
     //Insert operation in employee table
     
     $sql = "INSERT INTO employee(first_name, middle_name, last_name, prefix, dob, gender, marital_status, employer, employment, image) VALUES('$first_name' , '$middle_name' , '$last_name' , '$prefix' , '$dob' , '$gender' , '$marital_status' , '$employer' , '$employment' , '$image');";
     
+
     $result = mysqli_query($conn, $sql);
     
     if (TRUE === $result) {
@@ -93,16 +80,16 @@ if(isset($_POST['submit'])) {
 
         /*if($result == TRUE)
         {
-        	header("Location:simple.php");
+          header("Location:simple.php");
         }*/
 
        /* if ($error == false)
          {
-        	$mess = '<div class="alert alert-success">You are registered!!</div>';
+          $mess = '<div class="alert alert-success">You are registered!!</div>';
         }
         else
         {
-        	$mess = '<div class="alert alert-danger">Error, try again</div>';
+          $mess = '<div class="alert alert-danger">Error, try again</div>';
         }*/
 
     } else {
@@ -124,89 +111,96 @@ mysqli_close($conn);
       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
    </head>
    <body>
-
-   <h2><u>DISPLAY INFORMATION</u></h2>
-
-  
-   		<div class="table-responsive">
-      		<table class="table table-bordered">
-         		<thead>
-            		<tr>
-               			<td>First name</td>
-               			<td>Middle_name</td>
-               			<td>Last_name</td>
-               			<td>Prefix</td>
-               			<td>Gender</td>
-               			<td>DOB</td>
-               			<td>Marital Status</td>
-               			<td>Employer</td>
-               			<td>Employment</td>
-               			<td>Picture</td>
-               			<td>Official Address</td>
-               			<td>Residential Address</td>
-               			<td>Communication</td>
-               			<td>Edit</td>
-               			<td>Delete</td>
-            		</tr>
-         		</thead>
-         		<tbody>
-            	<?php
-               		$conn = mysqli_connect($hostname, $username, $pass, $db_name);
-
-               		//Display operation
-               
-               		$fetch = " SELECT *, (
-                          select GROUP_CONCAT(street, ' ', city, ' ', zip) AS official_address FROM address addr WHERE type = 'office' AND addr.employee_id = e.id
-                           ) as official_address,
-                          (
-	                      select GROUP_CONCAT(street, ' ', city, ' ', zip) AS official_address FROM address addrhome WHERE type = 'residence' AND addrhome.employee_id = e.id
-                          ) AS residential_address,
-                          (
-                           SELECT type FROM communication c
-                           WHERE c.employee_id = e.id
-                          ) AS communication_medium
-                          FROM employee e";
-               
-               
-                         $result = mysqli_query($conn,$fetch);
-               
-                     	if(mysqli_num_rows($result) > 0)
-                     	{
-                         	//output of the data
-                         	while($row = mysqli_fetch_assoc($result))
-                         	{
-                         		/*echo '<pre>';
-                         		print_r($row);
-                         		echo '</pre>';*/
-
-			                  echo "<tr>";
-			                   echo "<td>" . $row["first_name"]  ."</td>";
-			                   echo "<td>" . $row["middle_name"]  ."</td>";
-			                   echo "<td>" . $row["last_name"]  ."</td>";
-			                   echo "<td>" . $row["prefix"]  ."</td>";
-			                   echo "<td>" . $row["gender"]  ."</td>";
-			                   echo "<td>" . $row["dob"]  ."</td>";
-			                   echo "<td>" . $row["marital_status"]  ."</td>";
-			                   echo "<td>" . $row["employer"]  ."</td>";
-			                   echo "<td>" . $row["employment"]  ."</td>";
-			                   echo "<td>" . $row["image"]  ."</td>";
-			                   echo "<td>" . $row["official_address"]  ."</td>";
-			                   echo "<td>" . $row["residential_address"]  ."</td>";
-			                   echo "<td>" . $row["communication_medium"] . "</td>";
-			                   echo "<td>" . "<a> Edit</a>";
-			                   echo "<td>" . "<a> Delete</a>";
-
-			                  echo "</tr>";
-			                }
-		                }
-		                else
-		                {
-			                	echo "<br>Insert Record";
-		                }
-               		?>
-         		</tbody>
-      		</table>
- 	</div>
+      <h2><u>DISPLAY INFORMATION</u></h2>
+      <div class="row">
+         <div class="col-xs-12">
+            <form method="POST">
+               <div class="table-responsive">
+                  <table class="table table-bordered table-hover">
+                     <thead>
+                        <tr>
+                           <td>Name</td>
+                           <td>Prefix</td>
+                           <td>Gender</td>
+                           <td>DOB</td>
+                           <td>Marital Status</td>
+                           <td>Employer</td>
+                           <td>Employment</td>
+                           <td>Picture</td>
+                           <td>Official Address</td>
+                           <td>Residential Address</td>
+                           <td>Communication</td>
+                           <td>Edit</td>
+                           <td>Delete</td>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        <?php
+                           $conn = mysqli_connect($hostname, $username, $pass, $db_name);
+                           
+                           //Display operation
+                           
+                           $fetch = " SELECT *, (
+                                    select GROUP_CONCAT(street, ' ', city, ' ', zip) AS official_address FROM address addr WHERE type = 'office' AND addr.employee_id = e.id
+                                     ) as official_address,
+                                    (
+                                 select GROUP_CONCAT(street, ' ', city, ' ', zip) AS official_address FROM address addrhome WHERE type = 'residence' AND addrhome.employee_id = e.id
+                                    ) AS residential_address,
+                                    (
+                                     SELECT type FROM communication c
+                                     WHERE c.employee_id = e.id
+                                    ) AS communication_medium
+                                    FROM employee e";
+                           
+                           
+                                   $result = mysqli_query($conn,$fetch);
+                           
+                                if(mysqli_num_rows($result) > 0)
+                                {
+                                    //output of the data
+                                    while($row = mysqli_fetch_assoc($result))
+                                    {
+                                      /*echo '<pre>';
+                                      print_r($row);
+                                      echo '</pre>';*/
+                                $image = $row["image"];
+                                echo "<tr>";
+                                echo "<td>" . $row["first_name"]." ".$row["middle_name"]." ". $row["last_name"] ."</td>";
+                                echo "<td>" . $row["prefix"]  ."</td>";
+                                echo "<td>" . $row["gender"]  ."</td>";
+                                echo "<td>" . $row["dob"]  ."</td>";
+                                echo "<td>" . $row["marital_status"]  ."</td>";
+                                echo "<td>" . $row["employer"]  ."</td>";
+                                echo "<td>" . $row["employment"]  ."</td>";
+                           
+                                if (!empty($image))
+                                {
+                                  echo "<td>" . "<img src='profile/$image' width='45' height='45'>"  ."</td>";  
+                                }
+                                else
+                                {
+                                  echo "<td> no image </td>";
+                                }
+                                echo "<td>" . $row["official_address"]  ."</td>";
+                                echo "<td>" . $row["residential_address"]  ."</td>";
+                                echo "<td>" . $row["communication_medium"] . "</td>";
+                                echo "<td>" . "<button type='submit'><a href='simple.php?emp_id=" . $row['id'] . "'>Edit</a></button></td>";
+                                echo "<td>" . "<button type='submit'><a href='delete.php?emp_id=" . $row['id'] ."'>Delete</a></button></td>";
+                           
+                               echo "</tr>";
+                             }
+                            }
+                            else
+                            {
+                              echo "<br>Insert Record";
+                            }
+                           ?>
+                     </tbody>
+                  </table>
+               </div>
+            </form>
+         </div>
+      </div>
    </body>
 </html>
 
